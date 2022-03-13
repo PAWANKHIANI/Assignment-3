832d51c3172f7fbf547ce32f03b44fe559bb6318
PAWANKHIANI <96990280+PAWANKHIANI@users.noreply.github.com>
2022-03-13 10:27:45 +0500

    Add files via upload

---
+++ Assignment 3/main.dart
@@ -0,0 +1,16 @@
+void main() {
+  // Question # 03:
+
+  /* var list1 = [1, 2, 3, 4, 5, 6, 7];
+  var list2 = [3, 5, 6, 7, 9, 10];
+
+  List<int> difference = list1.toSet().difference(list2.toSet()).toList();
+  print(difference.toString());*/
+
+  //Question # 06
+  /*List<num> a = [7, 14, 21, 28, 35, 42, 49, 56, 63, 70];
+  List<num> b = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+  var data = Map.fromIterables(a, b);
+  print(data);*/
+}
